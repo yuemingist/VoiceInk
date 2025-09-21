964124d4c13088f9b5305077f3e7f0961b3b3b78
岳明 <yueming1985@gmail.com>
2025-09-21 15:10:01 +0800

    搭建了多端的框架

---
+++ VoiceInk iOS/App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Antares
+//
+//  Created by Antares on 2024/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Macos")
+    }
+    
+    
+}
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceInk iOS/App/VoiceInkApp.swift
@@ -0,0 +1,18 @@
+//
+//  VoiceInkApp.swift
+//  Antares
+//
+//  Created by Antares on 2024/7/16.
+//
+
+import SwiftUI
+
+@main
+struct VoiceInkApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
